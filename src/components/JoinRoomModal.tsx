15d46054bd9fb9938647db40223b8d560f776f9e
Shubham Mankar <mankarshubham81@gmail.com>
2025-01-07 17:34:17 +0530

    removed unused code

--- src/components/JoinRoomModal.tsx
+++ src/components/JoinRoomModal.tsx
@@ -26,14 +26,6 @@ export default function JoinRoomForm({ onJoin }: Props) {
     }
   };
 
-  // Show a loading state while checking localStorage during the first render
-  if (userName === null) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <div className="text-white">Loading...</div>
-      </div>
-    );
-  }
 
   // If userName exists, skip rendering the form
   if (userName) {
